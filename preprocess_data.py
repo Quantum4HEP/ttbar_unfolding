bcf43b56d3d7b74a45f38048a333e0a33cf87d0c
SimoneGasperini <simone.gasperini4@unibo.it>
2025-01-31 13:56:27 +0100

    Fix entire data preprocessing and unfolding workflow

--- preprocess_data.py
+++ preprocess_data.py
@@ -11,8 +11,7 @@ variables = [
     Variable(name="ttbar_mass", nbins=20, bounds=(300, 2000)),
 ]
 
-# Useful to cut events over a certain energy level
-en_min = 0
+min_energy = 0
 #########################################################################################
 
 
@@ -22,26 +21,24 @@ ROOT.gStyle.SetOptStat(0)
 # Open input ROOT file and get tree
 tree = uproot.open(input_rootfile_path)["particle_level"]
 
-# Open new output ROOT file and make directories
+# Open output ROOT file and make directories
 output_rootfile = ROOT.TFile(output_rootfile_path, "RECREATE")
 particle_dir = output_rootfile.mkdir("particle")
 parton_dir = output_rootfile.mkdir("parton")
 migration_dir = output_rootfile.mkdir("migration")
 
 # Get boolean mask to identify particle and/or parton events
-particle = get_numpy_array(tree=tree, varname="passed_particle_sel", dtype=bool)
-parton = get_numpy_array(tree=tree, varname="passed_parton_sel", dtype=bool)
-
+particle = get_numpy_array(tree, varname="passed_particle_sel", dtype=bool)
+parton = get_numpy_array(tree, varname="passed_parton_sel", dtype=bool)
 
 # Get events weight (negative for background process events)
 event_weight = get_numpy_array(tree=tree, varname="eventweight")
 
 for var in variables:
     # Get numpy arrays for particle and parton events
-    arr_particle = get_numpy_array(tree=tree, varname=var.name)
-    arr_parton = get_numpy_array(tree=tree, varname=var.name_parton)
-    arr_mass = get_numpy_array(tree=tree, varname="ttbar_mass_parton")
-    
+    arr_particle = get_numpy_array(tree, varname=var.name)
+    arr_parton = get_numpy_array(tree, varname=var.name_parton)
+    arr_mass = get_numpy_array(tree, varname="ttbar_mass_parton")
 
     # Set proper binning according to data distribution
     binning = find_binning(x=arr_parton, nbins=var.nbins, bounds=var.bounds)
@@ -53,7 +50,7 @@ for var in variables:
         binning=binning,
         data=arr_particle,
         weights=event_weight,
-        mask=particle & (arr_mass>en_min),
+        mask=particle & (arr_mass > min_energy),
     )
     th1_parton = create_histo(
         name=var.name_parton,
@@ -61,7 +58,7 @@ for var in variables:
         binning=binning,
         data=arr_parton,
         weights=event_weight,
-        mask=parton & (arr_mass>en_min),
+        mask=parton & (arr_mass > min_energy),
     )
 
     # Create and fill missed and fake events histograms
@@ -71,7 +68,7 @@ for var in variables:
         binning=binning,
         data=arr_parton,
         weights=event_weight,
-        mask=~particle & parton & (arr_mass>en_min),
+        mask=~particle & parton & (arr_mass > min_energy),
     )
     th1_fake = create_histo(
         name=var.name + "_fake",
@@ -79,7 +76,7 @@ for var in variables:
         binning=binning,
         data=arr_particle,
         weights=event_weight,
-        mask=particle & ~parton & (arr_mass>en_min),
+        mask=particle & ~parton & (arr_mass > min_energy),
     )
 
     # Create and fill migration matrix histogram (measured X truth)
@@ -90,7 +87,7 @@ for var in variables:
         data_reco=arr_particle,
         data_truth=arr_parton,
         weights=event_weight,
-        mask=particle & parton & (arr_mass>en_min),
+        mask=particle & parton & (arr_mass > min_energy),
     )
 
     # Write histograms to output ROOT file
---
+++ unfold_variables.py
@@ -0,0 +1,99 @@
+import ROOT
+import numpy as np
+from qunfold import QUnfolder, QPlotter
+from qunfold.root2numpy import *
+from utils import get_binning
+
+
+# Open input ROOT file
+root_file = ROOT.TFile.Open("ttbar_qunfold.root")
+
+# Consider left-most and right-most overflow bins
+overflow = True
+
+# Loop over variables to be unfolded
+for variable in ["c_thetap", "ttbar_mass"]:
+
+    # Get binning from ROOT histograms
+    binning = get_binning(root_file, variable=variable, overflow=overflow)
+
+    # Get all required data as ROOT histograms
+    th2_migration = root_file.Get(f"migration/{variable}_migration")
+    th1_measured = root_file.Get(f"particle/{variable}")
+    th1_fake = root_file.Get(f"particle/{variable}_fake")
+    th1_truth = root_file.Get(f"parton/{variable}_parton")
+    th1_miss = root_file.Get(f"parton/{variable}_miss")
+
+    # Apply corrections to consider miss and fake events
+    th1_truth = th1_truth - th1_miss
+    th1_measured = th1_measured - th1_fake
+
+    # Convert ROOT histograms to numpy arrays
+    response = TH2_to_numpy(th2_migration, overflow=overflow)
+    measured = TH1_to_numpy(th1_measured, overflow=overflow)
+    truth = TH1_to_numpy(th1_truth, overflow=overflow)
+
+    # Normalize the response(=migration) matrix
+    norms = np.sum(response, axis=0)
+    norms[norms == 0] = 1e-10
+    response = response / norms
+
+    # Create the RooUnfoldResponse object
+    empty = th1_measured.Clone("h")
+    empty.Reset()
+    roo_response = ROOT.RooUnfoldResponse(empty, empty, th2_migration)
+    roo_response.UseOverflow(overflow)
+
+    # Run Matrix Inversion (MI) unfolding
+    unfolder = ROOT.RooUnfoldInvert(roo_response, th1_measured)
+    roo_unfolded = unfolder.Hunfold()
+    roo_covariance = unfolder.Eunfold()
+    unfolded = TH1_to_numpy(roo_unfolded, overflow=overflow)
+    covariance = TMatrix_to_numpy(roo_covariance)
+    qplotter = QPlotter(
+        response=response,
+        measured=measured,
+        truth=truth,
+        unfolded=unfolded,
+        covariance=covariance,
+        binning=binning,
+        method="MI",
+        norm=False
+    )
+    qplotter.show_histograms()
+
+    # Run Iterative Bayesian Unfolding (IBU)
+    unfolder = ROOT.RooUnfoldBayes(roo_response, th1_measured)
+    unfolder.SetIterations(4)
+    unfolder.SetVerbose(0)
+    roo_unfolded = unfolder.Hunfold()
+    roo_covariance = unfolder.Eunfold()
+    unfolded = TH1_to_numpy(roo_unfolded, overflow=overflow)
+    covariance = TMatrix_to_numpy(roo_covariance)
+    qplotter = QPlotter(
+        response=response,
+        measured=measured,
+        truth=truth,
+        unfolded=unfolded,
+        covariance=covariance,
+        binning=binning,
+        method="IBU",
+        norm=False
+    )
+    qplotter.show_histograms()
+
+    # Run the QUnfold algorithm using Gurobi solver
+    qunfolder = QUnfolder(
+        response=response, measured=measured, binning=binning, lam=0.0)
+    unfolded, covariance = qunfolder.solve_gurobi_integer()
+    qplotter = QPlotter(
+        response=response,
+        measured=measured,
+        truth=truth,
+        unfolded=unfolded,
+        covariance=covariance,
+        binning=binning,
+        method="GRB",
+        norm=False
+    )
+    qplotter.show_histograms()
